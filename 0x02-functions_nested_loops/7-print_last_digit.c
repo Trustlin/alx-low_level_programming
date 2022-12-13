49d0ef30e57579f11b45d8cb174ab90c643337f0
Trustlin <esosuakpotrustee@gmail.com>
2022-12-13 13:33:11 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n: number
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld <= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
