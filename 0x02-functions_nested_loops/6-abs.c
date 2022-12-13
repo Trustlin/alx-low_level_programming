c3bd4d1cdd55d65d1f63819a476be030eca8b15b
Trustlin <esosuakpotrustee@gmail.com>
2022-12-13 13:22:28 -0800

    abs program

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @t: integar to compute
+ * Return: absolute value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
