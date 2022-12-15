7a2c39133f20261f3a8c5a12f17caa15371435c7
Trustlin <esosuakpotrustee@gmail.com>
2022-12-15 13:14:21 -0800

    nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14
+ *
+ * Return: 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+
+				_putchar((i / 10) +  '0');
+
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
