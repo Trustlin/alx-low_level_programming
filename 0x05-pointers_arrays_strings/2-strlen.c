db6a9b4da4797b2b15e4f6141bcbdaabcf9d6472
Trustlin <esosuakpotrustee@gmail.com>
2022-12-19 12:43:57 -0800

    very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: index string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+
+		return(index);
+}
