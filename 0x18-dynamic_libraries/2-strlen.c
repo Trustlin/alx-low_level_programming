cacd5627eec9274e086a2558ce3b77694e2d5b67
Trustlin <esosuakpotrustee@gmail.com>
2023-03-20 13:00:58 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: index string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;/*start from 0*/
+
+	for (; *s++;)
+		a++;
+		return (a);
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: input string
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
